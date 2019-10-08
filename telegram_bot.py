87981cf5a69a139bb0387c7d793f3e87cbb87774
Ubuntu <yayan@tgbot.hv3ecyrnkwyepi4m15eb0hq3lb.fx.internal.cloudapp.net>
2019-10-08 20:09:42 +0000

    kek

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,30 @@
-import telebot;
-bot = telebot.TeleBot('679851125:AAHtjabEfaWIV5oKKJj-LAc_W5ESawLUd0I') 
+import telebot
 
+bot = telebot.TeleBot('679851125:AAHtjabEfaWIV5oKKJj-LAc_W5ESawLUd0I')
 
+def get_nick(s):
+    return s[s.find("add ") + 4:]
 
+def get_url(s):
+    return s[s.find("like ") + 5:]
+
+def get_likes(name):
+    return 2
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    name = message.from_user.username #id
+    if "add" in message.text:
+        nick_name = get_nick(message.text)
+        bot.send_message(message.chat.id, name + 'added user with nick: ' + nick_name)
+    elif "like" in message.text:
+        free_likes = get_likes(name)
+        if free_likes >= 2:
+            url = get_url(message.text)
+            bot.send_message(message.chat.id, name + 'added url to likes: ' + url)
+        else:
+            bot.send_message(message.chat.id, name + " you dont have a likes to continue this operation")
+    
+    print(name)
+    
+bot.polling()

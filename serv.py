cd223649e7ccbefbc5dac803dbe4f3c1501de789
YanShlyakhov <noname2012new@mail.ru>
2019-10-08 22:57:57 +0300

    first commit

--- serv.py
+++ serv.py
@@ -1,12 +1,29 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, abort
+import time
 app = Flask(__name__)
 
 task_str = "https://dribbble.com/shots/7150930-Social-Landing-page"
+
+names_ready = {}
+
+names_for_work = []
+
+users_online = {}
+
+online = 0
+
+def get_names():
+    global names_for_work
+    names_for_work = []
+    f = open("logins.txt", "r")
+    for login in f.readlines():
+        names_for_work.append(login[:-1])
 
 @app.route('/task_add', methods=['POST'])
 def handle_data():
     global task_str
     task_str = request.form['task']
+    get_names()
     return ""
 
 @app.route('/')
@@ -18,9 +35,35 @@ def api_root():
 </form>
             """
 
-@app.route('/task')
-def api_articles():
+def online_users():
+    global users_online
+    global online
+    now = time.clock()
+    for user in users_online:
+        if now - user.value() < 180000:
+            online -= 1
+
+@app.route('/task/<username>')
+def api_articles(username):
     global task_str
+    global names_ready
+    global online
+    global users_online
+    print(username)
+    online_users()
+    if username not in names_for_work:
+        abort(404)
+
+    if username not in names_ready.keys():
+        names_ready[username] = []
+        users_online[username] = time.clock()
+        online += 1
+        
+    if task_str in names_ready[username]:
+        abort(404)
+
+    users_online[username] = time.clock()
+    names_ready[username].append(task_str)
     return task_str
 
 if __name__ == '__main__':
---
+++ telegram_bot.py
@@ -0,0 +1,5 @@
+import telebot;
+bot = telebot.TeleBot('679851125:AAHtjabEfaWIV5oKKJj-LAc_W5ESawLUd0I') 
+
+
+
